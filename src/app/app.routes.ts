9ebe14fdcb659f40a8dc1e4b301339b093dde89f
Usman Jamil <UJamil@lmkr.com>
2025-06-24 16:09:19 +0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+
+import { HomeComponent } from './pages/home/home.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BlogEditorComponent } from './pages/blog-editor/blog-editor.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog/:id', component: BlogDetailComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'editor', component: BlogEditorComponent },
+  { path: '**', component: NotFoundComponent }
+];
